4f6e3bce75af6244ec62e47a8bbc235dcf62d8de
Breno Tardelli <brenot.dev@gmail.com>
2025-04-29 20:40:36 +0200

    wrong text fixed

--- src/App.jsx
+++ src/App.jsx
@@ -202,7 +202,7 @@ function Box({ children }) {
   return (
     <div className="box">
       <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
-        {isOpen ? "`–" : "+"}
+        {isOpen ? "–" : "+"}
       </button>
       {isOpen && children}
     </div>
